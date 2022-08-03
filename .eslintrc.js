f8421604c5bf9870e8efedd49c3cd7bcecd56937
Christopher Jones <tkcwebdev@gmail.com>
2022-08-03 14:25:08 -0700

    1: Install eslint dependensices

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["plugin:react/recommended", "xo"],
+	parser: ["@typescript-eslint/parser", "@babel/eslint-parser"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		semi: ["error", "always"],
+		quotes: ["error", "double"],
+	},
+};
