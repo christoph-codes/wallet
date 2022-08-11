4226cb793e4783649cb457293ae31a7ed386d2f9
Christopher Jones <tkcwebdev@gmail.com>
2022-08-11 13:02:41 -0700

    1: Add credit card index initializers for the data types

--- backend/redis/repositories/index.ts
+++ backend/redis/repositories/index.ts
@@ -4,4 +4,4 @@ import { creditCardSchema, userSchema } from "../schema/index.js";
 export const userRepository = client.fetchRepository(userSchema);
 await userRepository.createIndex();
 export const creditCardRepository = client.fetchRepository(creditCardSchema);
-// TODO: Create credit card index
+await creditCardRepository.createIndex();
