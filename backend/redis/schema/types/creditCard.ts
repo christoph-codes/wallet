e2de438f1ed45aa4f5a6d758f644e2fe247c3e76
Christopher Jones <tkcwebdev@gmail.com>
2022-08-11 17:06:44 -0700

    1: Create credit card interface and add due date to the schema type

--- backend/redis/schema/types/creditCard.ts
+++ backend/redis/schema/types/creditCard.ts
@@ -1,10 +1,21 @@
-import { SchemaDefinition } from "redis-om";
+import { Entity, SchemaDefinition } from "redis-om";
+
+export interface ICard extends Entity {
+	name?: string;
+	issuer?: 'visa' | 'mastercard' | 'discover' | 'amex';
+	limit?: number;
+	balance?: number;
+	dueDate?: Date;
+	expirationDate?: string;
+	lastFour?: string;
+}
 
 export const creditCardType: SchemaDefinition = {
 	name: { type: 'string' },
 	issuer: { type: 'string' },
 	limit: { type: 'number' },
 	balance: { type: 'number' },
+	dueDate: { type: 'date'},
 	expirationDate: { type: 'string' },
 	lastFour: { type: 'string' },
 };
