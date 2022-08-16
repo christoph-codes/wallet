e35b10d773b61b56eb15a1e998ae514e41b9ee9d
Christopher Jones <tkcwebdev@gmail.com>
2022-08-15 23:31:29 -0700

    main: Add login and create account pages to router

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import CreateAccount from "../pages/CreateAccount";
+import Login from "../pages/Login";
 import Home from "../pages/Home";
 import "./App.scss";
 
 function App() {
 	return (
 		<Routes>
+			<Route path="/create-account" element={<CreateAccount />} />
+			<Route path="/login" element={<Login />} />
 			<Route path="/" element={<Home />} />
 		</Routes>
 	);
