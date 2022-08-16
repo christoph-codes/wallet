03a34a88f80cbdc9a70d558da790d3a4e5db1101
Christopher Jones <tkcwebdev@gmail.com>
2022-08-15 23:30:42 -0700

    main: Create page template and include React helmet for seo and metadata. Still needs styling. WIP

---
+++ frontend/src/templates/Page/Page.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import { Helmet } from "react-helmet";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import "./Page.scss";
+
+export interface IPageProps {
+	className?: string;
+	title?: string;
+	description?: string;
+	children?: ReactNode;
+}
+
+const Page = ({
+	className,
+	title = "Wallet » A credit card wallet for all of your credit cards.",
+	description = "A credit card wallet for all of your credit cards.",
+	children,
+}: IPageProps) => {
+	return (
+		<>
+			<Helmet>
+				<title>{title}</title>
+				<meta name="description" content={description} />
+			</Helmet>
+			<Header />
+			<main className={`Page ${className || ""}`}>{children}</main>
+			<Footer />
+		</>
+	);
+};
+
+export default Page;
