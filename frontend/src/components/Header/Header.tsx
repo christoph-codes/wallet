d3dbd1dd3005f55d16ce87940827babe5c44e935
Christopher Jones <tkcwebdev@gmail.com>
2022-08-17 01:04:08 -0700

    3: Create dummy info for the header to help with navigating throughout auth and non-auth pages. WIP

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,20 @@
+import { useAuth } from "../../providers/AuthProvider";
 import "./Header";
 
 const Header = () => {
-	return <header>Header</header>;
+	const { user, logout } = useAuth();
+	return (
+		<header>
+			Hello, {user?.fname}
+			<div className="actions">
+				<a href="/create-account">Create Account</a>
+				{` | `}
+				<a href="/login">Login</a>
+				{` | `}
+				<button onClick={() => logout && logout()}>Logout</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
