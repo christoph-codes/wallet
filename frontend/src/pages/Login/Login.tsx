f140e88bea106b1b94f072e7c3299feba0ec6918
Christopher Jones <tkcwebdev@gmail.com>
2022-08-17 01:01:10 -0700

    3: Setup redirect for any logged in user to the dashboard, connect new functions with login form

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,14 +1,16 @@
 import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../providers/AuthProvider";
 import Page from "../../templates/Page";
 
 const Login = () => {
-	const { login } = useAuth();
+	const navigate = useNavigate();
+	const { login, user } = useAuth();
 	const [error, setError] = useState("");
 	const submitLogin = (e: ChangeEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const form: FormData = new FormData(e.target);
-		const formData = Object.fromEntries(form.entries());
+		const formData: any = Object.fromEntries(form.entries());
 		if (!formData.email || !formData.password) {
 			setError("You must fill out all required fields.");
 		} else {
@@ -17,6 +19,9 @@ const Login = () => {
 			e.target.reset();
 		}
 	};
+	if (user?.authId) {
+		navigate("/dashboard");
+	}
 	return (
 		<Page className="Login">
 			<form onSubmit={submitLogin}>
