b576f945eee8cd91c289a16ffa4e24a6ad67a833
Christopher Jones <tkcwebdev@gmail.com>
2022-08-17 01:02:28 -0700

    3: Setup all authentication functions to be exported as well as setup localstorage for persisting a logged in user for insance database calls for authentication data, and more...

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,4 +1,7 @@
-import { UserInfo } from "firebase/auth";
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
 import {
 	createContext,
 	useContext,
@@ -6,16 +9,28 @@ import {
 	ReactNode,
 	useEffect,
 } from "react";
+import { Navigate } from "react-router-dom";
 import { auth } from "../config/firebase";
+import { db } from "../config/server";
+import { clearItem, getWithExpiry, setWithExpiry } from "../utils/helpers";
 
-export interface IUser extends UserInfo {
+export interface ICreateAccountArgs {
+	fname: string;
+	lname: string;
+	email: string;
+	password: string;
+}
+export interface IUser {
 	fname?: string;
 	lname?: string;
+	email?: string;
+	authId?: string;
 	cards?: string[];
 }
 
 export interface IAuthContext {
-	login?: (email: FormDataEntryValue, password: FormDataEntryValue) => void;
+	createAccount?: (user: ICreateAccountArgs) => void;
+	login?: (email: string, password: string) => void;
 	logout?: () => void;
 	user?: IUser;
 }
@@ -24,45 +39,57 @@ export interface IAuthProvider {
 }
 
 const AuthContext = createContext<IAuthContext>({
+	createAccount: () => {},
 	login: () => {},
 	logout: () => {},
 	user: {
-		uid: "",
 		fname: "",
 		lname: "",
-		cards: [],
-		displayName: "",
 		email: "",
-		phoneNumber: "",
-		photoURL: "",
-		providerId: "",
+		authId: "",
+		cards: [],
 	},
 });
 
 const AuthProvider = ({ children }: IAuthProvider) => {
 	const [user, setUser] = useState(() => {
-		console.log("current user: ", auth.currentUser);
-		return auth.currentUser
-			? auth.currentUser
-			: {
-					uid: "",
-					fname: "",
-					lname: "",
-					cards: [],
-					displayName: "",
-					email: "",
-					phoneNumber: "",
-					photoURL: "",
-					providerId: "",
-			  };
+		const localUser = getWithExpiry("wallet_user");
+		return (
+			localUser || {
+				fname: "",
+				lname: "",
+				email: "",
+				authId: "",
+				cards: [],
+			}
+		);
 	});
 
 	useEffect(() => {
-		auth.onAuthStateChanged((firebaseUser) => {
-			if (firebaseUser) {
-				setUser(firebaseUser);
-			}
-		});
+		setWithExpiry("wallet_user", user, 3600000);
+	}, [user]);
+
+	useEffect(() => {
+		if (!user.authId)
+			auth.onAuthStateChanged((firebaseUser) => {
+				if (firebaseUser) {
+					db.post(`/users/get`, { authId: firebaseUser?.uid })
+						.then((redisUser) => {
+							setUser(redisUser.data);
+						})
+						.catch((err) => {
+							throw new Error(err);
+						});
+				} else {
+					setUser({
+						fname: "",
+						lname: "",
+						email: "",
+						authId: "",
+						cards: [],
+					});
+				}
+			});
 	}, []);
 
 	/**
@@ -71,16 +98,72 @@ const AuthProvider = ({ children }: IAuthProvider) => {
 	 * @param email `string` in email format associated with the users account
 	 * @param password a secret `string` that is associated with the users account
 	 */
-	const login = (email: FormDataEntryValue, password: FormDataEntryValue) => {
+	const login = (email: string, password: string) => {
 		console.log("creds", email, password);
+		signInWithEmailAndPassword(auth, email, password).then(
+			(firebaseUser) => {
+				if (firebaseUser) {
+					console.log("firebaseUser", firebaseUser);
+					db.post(`/users/get`, { authId: firebaseUser?.user?.uid })
+						.then((user) => {
+							if (user) {
+								setUser(user.data);
+							}
+						})
+						.catch((err) => {
+							console.log("err", err);
+						});
+				}
+			}
+		);
+	};
+
+	const createAccount = (user: ICreateAccountArgs) => {
+		const { fname, lname, email, password } = user;
+		if (!fname || !lname || !email || !password) {
+			throw new Error(
+				"You must provide valid credentials to create an account"
+			);
+		}
+		createUserWithEmailAndPassword(auth, email, password)
+			.then(async (response) => {
+				console.log("response:", response);
+				console.log("user:", user);
+				await db
+					.post("/users/create", {
+						user: {
+							fname: user.fname,
+							lname: user.lname,
+							email: response.user.email,
+							uid: response.user.uid,
+						},
+					})
+					.then((redisUser) => {
+						console.log("redisUser:", redisUser.data);
+						setUser(redisUser.data);
+					})
+					.catch((err) => {
+						console.log("failed db user create", err.code);
+						throw new Error(err);
+					});
+			})
+			.catch((error) => {
+				console.log("firebase create account error", error);
+				throw new Error(error.code);
+			});
 	};
 
 	const logout = () => {
 		console.log("logging out this user:", user);
+		clearItem("wallet_user");
 	};
 
+	if (user?.authId) {
+		return <Navigate to="/login" />;
+	}
+
 	return (
-		<AuthContext.Provider value={{ user, login, logout }}>
+		<AuthContext.Provider value={{ user, login, createAccount, logout }}>
 			{children}
 		</AuthContext.Provider>
 	);
