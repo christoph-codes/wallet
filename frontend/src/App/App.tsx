b838f74ab032e087fa8c57c4de8cbe3890e2cc20
Christopher Jones <tkcwebdev@gmail.com>
2022-08-17 22:39:06 -0700

    3: Create dashboard for dummy dashboard

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import CreateAccount from "../pages/CreateAccount";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 import "./App.scss";
 
 function App() {
 	return (
 		<Routes>
+			<Route path="/dashboard" element={<Dashboard />} />
 			<Route path="/create-account" element={<CreateAccount />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/" element={<Home />} />
