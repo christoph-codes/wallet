13225ad8f37ebea6d0b384b126d454933ae2e6af
Christopher Jones <tkcwebdev@gmail.com>
2022-08-18 22:24:12 -0700

    5: Started initial setup of the Dashboard with some content. Need to work on fetching the database for the card results.

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,15 +1,50 @@
+import { useEffect, useState } from "react";
+import CCard from "../../components/CCard";
+import { ICreditCard } from "../../components/CCard/CCard";
+import { db } from "../../config/server";
 import { useAuth } from "../../providers/AuthProvider";
-import Page from "../../templates/Page";
+import DashboardTemplate from "../../templates/Dashboard";
 import "./Dashboard.scss";
+
+export interface ICard {
+	name?: string;
+	issuer?: "visa" | "mastercard" | "discover" | "amex";
+	limit?: number;
+	balance?: number;
+	dueDate?: Date;
+	expirationDate?: string;
+	lastFour?: string;
+}
 
 const Dashboard = () => {
 	const { user } = useAuth();
+	const [usersCards, setUsersCards] = useState<ICard[]>([]);
+	useEffect(() => 
+		 async () => {
+			const id = await db.post("/users/get", { authId: user?.authId });
+			return await db
+				.get("/cards", id)
+				.then((cards) => setUsersCards(cards));
+		};
+	);
 	return (
-		<Page title="User Dashboard">
-			<h1>
-				{user?.fname}, {user?.lname}
-			</h1>
-		</Page>
+		<DashboardTemplate className="Dashboard" user={user}>
+			{usersCards ? (
+				<>
+					<h1 className="Dashboard__available_balance">
+						${usersCards?.length.toFixed(2).toLocaleString()}
+						<span>Total Available Balance</span>
+					</h1>
+					<section className="Dashboard__cards">
+						{usersCards?.map((card, index) => {
+							return <CCard key={index} card={card} />;
+						})}
+					</section>
+				</>
+			) : (
+				"...Loading"
+			)}
+		</DashboardTemplate>
 	);
 };
 
