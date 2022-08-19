819eed74fd4f961c3c62e172dc26476b4b2f7cb4
Christopher Jones <tkcwebdev@gmail.com>
2022-08-18 22:21:39 -0700

    5: Create dashboard header

---
+++ frontend/src/components/DashboardHeader/DashboardHeader.tsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+import { useAuth } from "../../providers/AuthProvider";
+import { useOutsideAlerter } from "../../utils/callbacks";
+import "./DashboardHeader.scss";
+
+export interface IDashboardHeader {
+	name?: string;
+}
+
+const DashboardHeader = ({ name }: IDashboardHeader) => {
+	const { logout } = useAuth();
+	const dropdownRef = useRef(null);
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	useOutsideAlerter(dropdownRef, () =>
+		setDropdownOpen(dropdownOpen && false)
+	);
+	return (
+		<header className="DashboardHeader">
+			<button
+				className={`DashboardHeader__button ${
+					dropdownOpen ? "DashboardHeader__button--open" : ""
+				}`}
+				ref={dropdownRef}
+				onClick={() => setDropdownOpen(!dropdownOpen)}
+			>
+				{name}
+			</button>
+			<div
+				className={`DashboardHeader__dropdown ${
+					dropdownOpen ? "DashboardHeader__dropdown--open" : ""
+				}`}
+			>
+				<ul>
+					<li>
+						<a href="/dashboard/settings">Settings</a>
+					</li>
+					<li>
+						<button onClick={() => logout && logout()}>
+							Logout
+						</button>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
+};
+
+export default DashboardHeader;
