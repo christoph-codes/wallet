b8955ea7b8fb61f1459e0189823b2242567be9f3
Christopher Jones <tkcwebdev@gmail.com>
2022-08-20 17:16:29 -0700

    5: Add input button and modal to the main dashboard for adding a card

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import axios from "axios";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import Button from "../../components/Button";
 import CCard from "../../components/CCard";
-import { ICreditCard } from "../../components/CCard/CCard";
+import Input from "../../components/Input";
+import Modal from "../../components/Modal";
 import { db } from "../../config/server";
 import { useAuth } from "../../providers/AuthProvider";
 import DashboardTemplate from "../../templates/Dashboard";
@@ -18,29 +21,83 @@ export interface ICard {
 
 const Dashboard = () => {
 	const { user } = useAuth();
-	const [usersCards, setUsersCards] = useState<ICard[]>([]);
-	useEffect(() => 
-		 async () => {
-			const id = await db.post("/users/get", { authId: user?.authId });
+	const [usersCards, setUsersCards] = useState<ICard[] | null>(null);
+	/**
+	 * Getting a users cards
+	 */
+	useEffect(() => {
+		const unsubscribe = async () => {
 			return await db
-				.get("/cards", id)
-				.then((cards) => setUsersCards(cards));
+				.post("/cards", { userId: user?.entityId })
+				.then((cards) => {
+					console.log("getCards getter:", cards.data);
+					setUsersCards(cards.data);
+				})
+				.catch(console.log);
 		};
-	);
+		unsubscribe();
+	}, [user?.entityId]);
+
+	const [error, setError] = useState("");
+	const addCard = (e: ChangeEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setError("");
+		const form: FormData = new FormData(e.target);
+		const formData: any = Object.fromEntries(form.entries());
+		if (Object.values(formData).every((x) => x === "")) {
+			setError("You must fill out all required fields.");
+		} else {
+			const payload = {
+				card: formData,
+				userId: user?.entityId,
+			};
+			db.post("/cards/add", payload).then(console.log);
+			setError("");
+			e.target.reset();
+		}
+	};
 	return (
 		<DashboardTemplate className="Dashboard" user={user}>
+			<h1 className="Dashboard__available_balance">
+				<sup>$</sup>
+				{!usersCards
+					? Number(0).toFixed(2)
+					: usersCards?.length.toFixed(2)}
+				<span>Total Available Balance</span>
+			</h1>
 			{usersCards ? (
-				<>
-					<h1 className="Dashboard__available_balance">
-						${usersCards?.length.toFixed(2).toLocaleString()}
-						<span>Total Available Balance</span>
-					</h1>
-					<section className="Dashboard__cards">
-						{usersCards?.map((card, index) => {
+				<section className="Dashboard__cards">
+					{usersCards.length >= 1 ? (
+						usersCards?.map((card, index) => {
 							return <CCard key={index} card={card} />;
-						})}
-					</section>
-				</>
+						})
+					) : (
+						<div className="Dashboard__empty_cards">
+							<p>No Cards Have been added. Add one now!</p>
+							<Modal
+								buttonVariant="primary-outline"
+								buttonLabel="Add Card"
+							>
+								<h1>Hello World</h1>
+								<form onSubmit={addCard}>
+									{error && (
+										<p className="errorText">{error}</p>
+									)}
+									<Input name="cardName" label="Card Name" />
+									<Input
+										name="lastFour"
+										maxlength="4"
+										type="tel"
+										label="Last Four of Card Number"
+										placeholder="Last Four"
+									/>
+									<Input name="cardName" label="Card Name" />
+									<Button type="submit">Add Card</Button>
+								</form>
+							</Modal>
+						</div>
+					)}
+				</section>
 			) : (
 				"...Loading"
 			)}
