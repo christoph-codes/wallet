73c93b06834daf7511681443ad800bf3aa90ed29
Christopher Jones <tkcwebdev@gmail.com>
2022-08-22 16:14:00 -0700

    5: Update card art to include props based on data object passed in

--- frontend/src/components/CardArt/CardArt.tsx
+++ frontend/src/components/CardArt/CardArt.tsx
@@ -4,30 +4,58 @@ import {
 	SiMastercard,
 	SiDiscover,
 	SiAmericanexpress,
-	SiJcb,
 } from "react-icons/si";
+import { invertColor } from "../../utils/helpers";
 import "./CardArt.scss";
 
 export interface ICardArt {
-	bgColor?: string;
-	issuer: ICard["issuer"];
 	className?: string;
+	data?: ICard;
 	[x: string]: any;
 }
-const CardArt = ({ bgColor, issuer, className }: ICardArt) => {
+const CardArt = ({ data, className }: ICardArt) => {
+	const defaultBG = "#4444ff";
+	const color = invertColor(data?.bgColor || defaultBG);
+	const issuerLogoSize = 64;
 	const issuerArt = {
-		visa: <SiVisa size="32" />,
-		mastercard: <SiMastercard size="32" />,
-		discover: <SiDiscover size="32" />,
-		amex: <SiAmericanexpress size="32" />,
-		jcb: <SiJcb size="32" />,
+		visa: <SiVisa color={color} size={issuerLogoSize} />,
+		mastercard: <SiMastercard color={color} size={issuerLogoSize} />,
+		discover: <SiDiscover color={color} size={issuerLogoSize} />,
+		amex: <SiAmericanexpress color={color} size={issuerLogoSize} />,
 	};
+	console.log("issuer", data?.issuer);
 	return (
 		<div
-			style={{ backgroundColor: bgColor }}
+			style={{ backgroundColor: data?.bgColor || defaultBG }}
 			className={`CardArt ${className || ""}`}
 		>
-			{issuerArt[issuer as keyof typeof issuerArt]}
+			<div className="CardArt__container">
+				<h1 className="CardArt__numbers" style={{ color: color }}>
+					{data?.lastFour?.substring(data?.lastFour?.length - 4)}
+					<span>{data?.name}</span>
+				</h1>
+				<div className="CardArt__details" style={{ color: color }}>
+					{data?.expirationDate && (
+						<>
+							<p>
+								Exp:{" "}
+								<strong>
+									{data?.expirationDate?.substring(2, 0) +
+										"/" +
+										data?.expirationDate?.substring(
+											2,
+											data?.expirationDate?.length
+										)}
+								</strong>
+							</p>
+							<p>
+								CVV: <strong>{data?.cvv}</strong>
+							</p>
+						</>
+					)}
+				</div>
+				{issuerArt[data?.issuer as keyof typeof issuerArt]}
+			</div>
 		</div>
 	);
 };
