7d007f7f8a8e2b32ef3aabf143f5b15f305c9348
Christopher Jones <tkcwebdev@gmail.com>
2022-08-23 00:57:56 -0700

    5: Lint formatting issues in helpers function file

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -37,38 +37,36 @@ export const clearItem = (key: string) => {
 };
 
 export const invertColor = (hex: string) => {
-    if (hex?.indexOf('#') === 0) {
-        hex = hex.slice(1);
-    }
-    // convert 3-digit hex to 6-digits.
-    if (hex?.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-    }
-    if (hex?.length !== 6) {
-        throw new Error('Invalid HEX color.');
+	if (hex?.indexOf("#") === 0) {
+		hex = hex.slice(1);
+	}
+	// convert 3-digit hex to 6-digits.
+	if (hex?.length === 3) {
+		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+	}
+	if (hex?.length !== 6) {
+		throw new Error("Invalid HEX color.");
 	}
 	const r = parseInt(hex.slice(0, 2), 16);
 	const g = parseInt(hex.slice(2, 4), 16);
 	const b = parseInt(hex.slice(4, 6), 16);
-    // Credit: https://stackoverflow.com/a/3943023/112731
-    return (r * 0.299 + g * 0.587 + b * 0.114) > 186
-        ? '#000000'
-        : '#FFFFFF';
-}
+	// Credit: https://stackoverflow.com/a/3943023/112731
+	return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
+};
 
 export const validateCreditCardNumber = (value: string) => {
 	let visaRegEx = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
 	let mastercardRegEx = /^(?:5[1-5][0-9]{14})$/;
 	let amexpRegEx = /^(?:3[47][0-9]{13})$/;
 	let discovRegEx = /^(?:6(?:011|5[0-9][0-9])[0-9]{12})$/;
-  
+
 	if (visaRegEx.test(value)) {
-	  return 'visa'
+		return "visa";
 	} else if (mastercardRegEx.test(value)) {
-	  return 'mastercard'
+		return "mastercard";
 	} else if (amexpRegEx.test(value)) {
-	  return 'amex'
+		return "amex";
 	} else if (discovRegEx.test(value)) {
-	  return 'discover'
+		return "discover";
 	}
   }
