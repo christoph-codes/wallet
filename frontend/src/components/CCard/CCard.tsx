170eb3e63796d8d4f0302aac025300e0ce3c60b9
Christopher Jones <tkcwebdev@gmail.com>
2022-08-23 00:54:40 -0700

    5: Add logic and styling to the CCard component and use CardArt component inside

--- frontend/src/components/CCard/CCard.tsx
+++ frontend/src/components/CCard/CCard.tsx
@@ -1,27 +1,38 @@
+import { ICard } from "../../pages/Dashboard/Dashboard";
+import { invertColor, numberToCurrency } from "../../utils/helpers";
+import Button from "../Button";
+import CardArt from "../CardArt";
 import "./CCard.scss";
 
-export interface ICreditCard {
-	name?: string;
-	availableBalance?: number;
-	art?: string;
-}
-
 export interface ICCard {
-	card?: ICreditCard;
+	card?: ICard;
 	className?: string;
 }
 
 const CCard = ({ className, card }: ICCard) => {
+	console.log("card", card);
+	const color = card?.bgColor
+		? invertColor(card?.bgColor)
+		: invertColor("#1042E6");
 	return (
 		<article className="CCard">
-			<div className="CCard__bg">
-				<h3>
-					{card?.availableBalance?.toFixed(2)}
+			<div
+				style={{
+					backgroundColor: card?.bgColor || "#1042E6",
+					color: color,
+				}}
+				className="CCard__bg"
+			>
+				<h3 style={{ color: color }}>
+					<sup>$</sup>
+					{card?.balance && numberToCurrency(card?.balance)}
 					<span>Available Balance</span>
 				</h3>
 			</div>
-			<img src={card?.art} alt={card?.name} />
-			<button className="CCard__cta">View</button>
+			<div className="CCard__content">
+				<CardArt data={card} />
+				<Button className="CCard__cta">View Card</Button>
+			</div>
 		</article>
 	);
 };
