bd9476bd2d1e9b2fbdb152b8948cbdc212751372
Christopher Jones <tkcwebdev@gmail.com>
2022-08-23 00:52:29 -0700

    5: Add cvv and bgColor to the api functions

--- backend/redis/functions/cards.ts
+++ backend/redis/functions/cards.ts
@@ -13,7 +13,7 @@ export const addCard = async (card: ICard, userId: string) => {
 	if (!userId) {
 		throw new Error('A valid user must be logged in.');
 	}
-	if (!card.name && !card.issuer && !card.limit && !card.dueDate && !card.lastFour && !card.expirationDate && !card.balance) {
+	if (!card.name && !card.issuer && !card.limit && !card.dueDate && !card.lastFour && !card.expirationDate && !card.balance && !card.cvv && !card.bgColor) {
 		throw new Error('You must prrovide valid credit card data to add this card to your account.');
 	}
 	const created = await creditCardRepository.createAndSave({
@@ -24,6 +24,8 @@ export const addCard = async (card: ICard, userId: string) => {
 		lastFour: card.lastFour,
 		expirationDate: card.expirationDate,
 		balance: card.balance,
+		cvv: card.cvv,
+		bgColor: card.bgColor,
 	}).then(async res => {
 		console.log(res.entityId);
 		const user = await userRepository.fetch(userId);
