5837ea41a82b06b3432ee304ae223e3ee127f7ca
Christopher Jones <tkcwebdev@gmail.com>
2022-08-24 12:33:46 -0700

    5: Setup for heroku deployment

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,25 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
+import express from "express";
+import * as dotenv from "dotenv";
 dotenv.config();
-import routes from './routes/index.js';
-import usersRouter from './routes/users.routes.js';
-import cardsRouter from './routes/creditCards.routes.js';
+import routes from "./routes/index.js";
+import usersRouter from "./routes/users.routes.js";
+import cardsRouter from "./routes/creditCards.routes.js";
+import client from "./redis/index.js";
 
-console.log('process', process.env.REDIS_URL);
-console.log('chris', process.env.DEV);
+console.log("process", process.env.REDIS_URL);
+console.log("chris", process.env.DEV);
 
 const app = express();
 const port = 5000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(function (req, res, next) {
 	// res.header('Access-Control-Allow-Origin', `http://localhost:3000`); // update to match the domain you will make the request from
 	res.header({
-		'Access-Control-Allow-Origin': '*',
-		'Content-Type': 'application/json',
-		'Access-Control-Allow-Headers': '*',
+		"Access-Control-Allow-Origin": "*",
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Headers": "*",
 	});
 	next();
 });
@@ -28,12 +29,14 @@ app.use(function (req, res, next) {
 // });
 
 // users route
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 // credit card route
-app.use('/cards', cardsRouter);
+app.use("/cards", cardsRouter);
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+	return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+await client.close();
--- backend/redis/index.ts
+++ backend/redis/index.ts
@@ -1,10 +1,17 @@
-import { Client } from 'redis-om';
-import 'dotenv/config';
+import { Client } from "redis-om";
+import "dotenv/config";
 
 /* pulls the Redis URL from .env */
-const url = process.env.NODE_ENV === 'development' ? 'redis://localhost:6379' : process.env.REDIS_URL;
-console.log('url', process.env.REDIS_URL);
+const url =
+	process.env.NODE_ENV === "development"
+		? "redis://localhost:6379"
+		: process.env.REDIS_URL;
+console.log("url", url);
 /* create and open the Redis OM Client */
-const client = await new Client().open(url)
+const client = await new Client().open(url);
+
+console.log("client", client);
+
+// await client.close();
 
 export default client;
