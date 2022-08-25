0ab3a977f948ec94d4378ad9580610a98df61a6e
Christopher Jones <tkcwebdev@gmail.com>
2022-08-25 00:32:45 -0700

    5: Restructure backend to live in the root directory

--- redis/functions/cards.ts
+++ redis/functions/cards.ts
@@ -1,6 +1,6 @@
-import { ICard } from './../schema/types/creditCard';
+import { ICard } from "../schema/types/creditCard";
 import { creditCardRepository, userRepository } from "../repositories/index.js";
-import client from '../index.js';
+import client from "../index.js";
 
 /**
  * Add card to redis function
@@ -9,35 +9,50 @@ import client from '../index.js';
  * @returns The created credit card and adds the generated card it do the loggedin user.
  */
 export const addCard = async (card: ICard, userId: string) => {
-	console.log('adding card');
+	console.log("adding card");
 	if (!userId) {
-		throw new Error('A valid user must be logged in.');
+		throw new Error("A valid user must be logged in.");
 	}
-	if (!card.name && !card.issuer && !card.limit && !card.dueDate && !card.lastFour && !card.expirationDate && !card.balance && !card.cvv && !card.bgColor) {
-		throw new Error('You must prrovide valid credit card data to add this card to your account.');
+	if (
+		!card.name &&
+		!card.issuer &&
+		!card.limit &&
+		!card.dueDate &&
+		!card.lastFour &&
+		!card.expirationDate &&
+		!card.balance &&
+		!card.cvv &&
+		!card.bgColor
+	) {
+		throw new Error(
+			"You must prrovide valid credit card data to add this card to your account."
+		);
 	}
-	const created = await creditCardRepository.createAndSave({
-		name: card.name,
-		issuer: card.issuer,
-		limit: card.limit,
-		dueDate: card.dueDate,
-		lastFour: card.lastFour,
-		expirationDate: card.expirationDate,
-		balance: card.balance,
-		cvv: card.cvv,
-		bgColor: card.bgColor,
-	}).then(async res => {
-		console.log(res.entityId);
-		const user = await userRepository.fetch(userId);
-		user.cards = [...user.cards,res.entityId];
-		await userRepository.save(user);
-		return {
-			userUpdated: user.entityId,
-			cardAdded: res.entityId,
-		}
-	}).catch(err => {
-		return err;
-	});
+	const created = await creditCardRepository
+		.createAndSave({
+			name: card.name,
+			issuer: card.issuer,
+			limit: card.limit,
+			dueDate: card.dueDate,
+			lastFour: card.lastFour,
+			expirationDate: card.expirationDate,
+			balance: card.balance,
+			cvv: card.cvv,
+			bgColor: card.bgColor,
+		})
+		.then(async (res) => {
+			console.log(res.entityId);
+			const user = await userRepository.fetch(userId);
+			user.cards = [...user.cards, res.entityId];
+			await userRepository.save(user);
+			return {
+				userUpdated: user.entityId,
+				cardAdded: res.entityId,
+			};
+		})
+		.catch((err) => {
+			return err;
+		});
 	return created;
 };
 
@@ -49,9 +64,9 @@ export const addCard = async (card: ICard, userId: string) => {
  */
 export const updateCard = async (cardId: string, updatedCard: ICard) => {
 	const card = await creditCardRepository.fetch(cardId);
-	Object.entries(updatedCard).forEach(item => {
+	Object.entries(updatedCard).forEach((item) => {
 		card[item[0]] = item[1];
-	})
+	});
 	const id = await creditCardRepository.save(card);
 	return id;
 };
@@ -62,16 +77,16 @@ export const updateCard = async (cardId: string, updatedCard: ICard) => {
  * @returns an array of credit cards associated to the users account.
  */
 export const getUsersCards = async (userId: string) => {
-	const user = await userRepository.fetch(userId); 
-	
+	const user = await userRepository.fetch(userId);
+
 	if ((user?.cards?.length || 0) < 1) {
 		return [];
 	}
-	const cards = user.cards?.map(async cardId => {
+	const cards = user.cards?.map(async (cardId) => {
 		return await creditCardRepository.fetch(cardId);
 	});
 	return await Promise.all(cards);
-}
+};
 
 /**
  * Get a single card from the redis database
@@ -81,16 +96,16 @@ export const getUsersCards = async (userId: string) => {
  */
 export const getCard = async (cardId: string, userId: string) => {
 	const user = await userRepository.fetch(userId);
-	const exists = await client.execute(['EXISTS', `User:${userId}`]);
+	const exists = await client.execute(["EXISTS", `User:${userId}`]);
 	if (!exists) {
-		throw new Error('This user does not exist');
+		throw new Error("This user does not exist");
 	}
 	if (!user?.cards?.includes(cardId)) {
-		throw new Error('This user does not have access to this card');
+		throw new Error("This user does not have access to this card");
 	}
 	const card = await creditCardRepository.fetch(cardId);
 	return card;
-}
+};
 
 /**
  * Delete a single card from the redis database
@@ -99,14 +114,14 @@ export const getCard = async (cardId: string, userId: string) => {
  */
 export const deleteCard = async (cardId: string, userId: string) => {
 	const user = await userRepository.fetch(userId);
-	const exists = await client.execute(['EXISTS', `User:${userId}`]);
+	const exists = await client.execute(["EXISTS", `User:${userId}`]);
 	if (!exists) {
-		throw new Error('This user does not exist');
+		throw new Error("This user does not exist");
 	}
 	if (!user.cards.includes(cardId)) {
-		throw new Error('This user does not have access to this card');
+		throw new Error("This user does not have access to this card");
 	}
 	const deletedCard = await creditCardRepository.remove(cardId);
 
 	return deletedCard;
-}
+};
