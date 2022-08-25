1456be3c52c2c8c1534b9764a8f8ad48bb42963e
Christopher Jones <tkcwebdev@gmail.com>
2022-08-25 01:54:46 -0700

    5: Update yml file to just install react scripts then build and remove import issue breaking build with cicd

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
