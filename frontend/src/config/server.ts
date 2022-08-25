339e345bb36d6b3fa5ae8dd4a3534985bce7df52
Christopher Jones <tkcwebdev@gmail.com>
2022-08-25 01:30:23 -0700

    5: Update development setup for the frontend

--- frontend/src/config/server.ts
+++ frontend/src/config/server.ts
@@ -1,13 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.SERVER_URL || 'http://localhost:5000/';
+const baseURL = process.env.SERVER_URL || "http://localhost:5000/";
+const clientURL = process.env.CLIENT_URL || "http://localhost:3000/";
 
 export const db = axios.create({
 	baseURL,
 	timeout: 1000,
 	headers: {
-		'Access-Control-Allow-Origin': 'http://localhost:3000/',
-		'Access-Control-Allow-Headers': '*',
-		'Content-Type': 'application/json',
-	}
-  });
+		"Access-Control-Allow-Origin": clientURL,
+		"Access-Control-Allow-Headers": "*",
+		"Content-Type": "application/json",
+	},
+});
