22ab26294c87469bc93aa3ba24b2052a17ee553d
Christopher Jones <tkcwebdev@gmail.com>
2022-08-29 21:10:40 -0700

    10: Remove prop for passing user object to add card page

--- frontend/src/pages/AddCard/AddCard.tsx
+++ frontend/src/pages/AddCard/AddCard.tsx
@@ -1,11 +1,9 @@
 import AddCardForm from "../../forms/AddCardForm";
-import { useAuth } from "../../providers/AuthProvider";
 import DashboardTemplate from "../../templates/Dashboard/DashboardTemplate";
 
 const AddCard = () => {
-	const { user } = useAuth();
 	return (
-		<DashboardTemplate className="Dashboard" user={user}>
+		<DashboardTemplate className="Dashboard">
 			<AddCardForm />
 		</DashboardTemplate>
 	);
